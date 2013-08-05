23a17de4adf11d8c41d9e478280ad5471bee0d1d
Manuel Gil <gikdew@gmail.com>
2013-08-05 21:01:13 +0200

    Progress (Cursor, distance2Points, collision)
    Added the cursor function to know where is the cursor each time, also it
    calculates de distance between 2 points and compares that with the
    radius of the circle, that is the way that I have to now where the
    player is dead.

--- game.js
+++ game.js
@@ -5,10 +5,14 @@ var centerY = canvas.height/2;
 var fps = 60;
 var counter = 0;
 var arriba = true;
+var cursorX;
+var cursorY;
+var frameCir;
+var velocity = 1;
 
 //Init some variables
 var circles = [];
-for(var i = 1; i<=190; i++){
+for(var i = 0; i<=190; i+=velocity){
 	circles.push({
 		x: centerX,
 		y: centerY,
@@ -19,27 +23,32 @@ for(var i = 1; i<=190; i++){
 //REMOVE
 for(var i in circles){
 	var circle = circles[i];
-	console.log(circle);
+	console.log(circle.rad);
 }
 
 function updateCircle(){
 	if(arriba){
 		counter++;	
-		console.log(counter);
+		//console.log(counter);
 		var circle = circles[counter - 1];
-		drawOneCircle(circle.x, circle.y, circle.rad, 'white');		
+		drawOneCircle(circle.x, circle.y, circle.rad, 'white');	
+		frameCir = circle.rad;
+		//console.log(frameCir);	
 		if(counter == circles.length - 1) {
 			arriba = false;
 			return;
+
 		}				
 	}
 
 	if(arriba == false){
 		var circle = circles[counter];
 		drawOneCircle(circle.x, circle.y, circle.rad, 'white');
+		frameCir = circle.rad;
+		//console.log(frameCir);
 		counter--;
 		if(counter == 0) arriba = true;
-		console.log(counter);
+		//console.log(counter);
 	}
 }
 
@@ -51,11 +60,46 @@ function drawBackground(){
 }
 	
 function drawOneCircle(x,y,r,color){
+	ctx.save();
 	ctx.beginPath();
 	ctx.arc(x, y, r, 0,2*Math.PI);
 	ctx.lineWidth = 1;
 	ctx.strokeStyle = color;
 	ctx.stroke();	
+	ctx.restore();
+}
+
+function drawOneLine(fX,fY){
+	ctx.save();
+	ctx.beginPath();
+    ctx.moveTo(centerX, centerY);
+    ctx.lineTo(fX, fY);
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = 'white';
+    ctx.stroke();
+    ctx.restore();
+}
+
+
+//MouseMove
+document.onmousemove = function(e){
+    cursorX = e.pageX;
+    cursorY = e.pageY;
+    //console.log(cursorX + ',' + cursorY);
+}
+
+function distance2Points(ax,ay,bx,by){
+	var numb = Math.pow((bx-ax),2)+Math.pow((by-ay), 2);
+	var distance = Math.sqrt(numb)
+	return Math.floor(distance);
+}
+
+function collision(ax,ay,bx,by,rad){
+	var dis = distance2Points(ax, ay, bx, by);
+	//console.log(dis);
+	if(rad <= dis){ //I can change that to == if I want to now the exact Point of Collision
+		console.log('collision');
+	}
 }
 
 //REMOVE
@@ -70,6 +114,8 @@ function get_random_color() {
 
 function gameTick(){
 	drawBackground();
+	drawOneLine(cursorX, cursorY);
+	collision(centerX, centerY, cursorX, cursorY, frameCir); //With the distance and the rad
 	updateCircle();
 }
 
